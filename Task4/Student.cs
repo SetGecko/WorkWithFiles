20e3da1c4e31128c0ac69dc1424e10ce89e8bff5
Алексей <setgecko@gmail.com>
2023-03-18 12:36:27 +0300

    Выполнил итоговые задания 8 модуля

---
+++ Task4/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    [Serializable]
+    public class Student
+    {
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
